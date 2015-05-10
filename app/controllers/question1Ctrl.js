b6e208f353ca7a206e171598515cfea0466b8bb2
yazman12 <yazman12@gmail.com>
2015-05-10 14:22:50 -0400

    question1 controller finished

--- app/controllers/question1Ctrl.js
+++ app/controllers/question1Ctrl.js
@@ -1,3 +1,41 @@
-module.exports = function (req, res) {
-    res.render('question1.jade', { title: 'Question 1' });
+var async = require('async'),
+    College = require('../models/College.js'),
+    Enrollment = require('../models/Enrollment.js');
+
+module.exports = function(req, res) {
+    var top10 = [];
+    Enrollment.findOne({}, function(err, data) {
+        if (err) 
+            throw err;
+
+        var array = data.enrollment.sort(function (a, b) {
+            if (a.EFTOTLT > b.EFTOTLT)
+                return 1;
+            if (a.EFTOTLT < b.EFTOTLT)
+                return -1;
+            return 0;
+        }).reverse();
+
+        array.splice(10);
+
+        async.forEach(array, function (school, callback) {
+            College.findOne({}, {
+                college: {
+                    $elemMatch: { UNITID: school.UNITID }
+                }
+            }, function (err, data) {
+                if (err) 
+                    throw err;
+                var topSchool = {};
+                topSchool.name = data.college[0].INSTNM;
+                topSchool.enrollment = school;
+                top10.push(topSchool);
+                callback();
+            });
+        }, function (err) {
+            if (err) 
+                throw err;
+            res.json(top10);
+        });
+    });
 };

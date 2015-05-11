201e58a14a61cd63408a3e150594dc5c8e3610d4
yazman12 <yazman12@gmail.com>
2015-05-10 22:12:49 -0400

    Removed see CSV route and updated views

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,7 +1,6 @@
 var getHomeCtrl = require('../controllers/getHome.js'),
 	getUploadCtrl = require('../controllers/getUpload.js'),
 	postUploadCtrl = require('../controllers/postUpload.js'),
-	getCsvCtrl = require('../controllers/getCsv.js'),
 	question1Ctrl = require('../controllers/question1Ctrl.js'),
 	question2Ctrl = require('../controllers/question2Ctrl.js'),
 	question3Ctrl = require('../controllers/question3Ctrl.js');
@@ -15,8 +14,6 @@ module.exports = function (app, express) {
 	appRouter.route('/upload') //upload CSV page
 		.get(getUploadCtrl) //GET requests
 		.post(postUploadCtrl); //POST requests
-
-	appRouter.get('/csv', getCsvCtrl); //see CSV files in DB
 
 	//Three questions
 	appRouter.get('/question1', question1Ctrl);
